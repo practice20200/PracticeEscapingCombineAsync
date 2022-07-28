01c551f24ef128d423f40e5c74155e697274a70f
practice20200 <wor9hard2018@gmail.com>
2022-07-27 20:49:20 -0700

    initial commit

---
+++ URLSessionEscapingANDCombine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  URLSessionEscapingANDCombine
+//
+//  Created by Apple New on 2022-07-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ URLSessionEscapingANDCombine/URLSessionEscapingANDCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSessionEscapingANDCombineApp.swift
+//  URLSessionEscapingANDCombine
+//
+//  Created by Apple New on 2022-07-27.
+//
+
+import SwiftUI
+
+@main
+struct URLSessionEscapingANDCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
